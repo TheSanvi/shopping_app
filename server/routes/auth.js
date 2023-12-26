a3f16a802bcfd9ea8c26096953ed98862b2dfa93
TheSanvi <sanvivermavns@gmail.com>
2023-12-26 20:02:04 +0530

    .

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const User = require("../models/user");
 
 const authRouter = express.Router();
 
-authRouter.post('/api/signup',(req,res)=>{
+authRouter.post('/api/signup',async  (req,res)=>{
 const {name, email,password}=req.body;
+
+const existingUser = await User.findOne({ email });
+if(existingUser){
+return res.status(400).json ({ msg: "User with same email already exists!"});
+}
+
+
 });
 
 module.exports = authRouter;
