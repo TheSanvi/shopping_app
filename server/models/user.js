543b91467bc0d0a8b1f7ab4b02162f3c50c5d99a
TheSanvi <sanvivermavns@gmail.com>
2023-12-27 12:20:00 +0530

    .

--- server/models/user.js
+++ server/models/user.js
@@ -1,37 +1,37 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-name:{
-requires:true,
-type:String,
-trim:true,
-},
-email:{
-required:true,
-type:String,
-trim:true,
-validate:{
-validator:(value)=>{
-const re =
-  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-return value.match(re);
-},
-message:"Please enter a valid email address",
-},
-},
-password:{
-required:true,
-type:String,
-},
-address:{
-type:String,
-default: '',
-},
-type:{
-type:String,
-default: 'user',
-},
+  name: {
+    required: true, // Corrected from requires
+    type: String,
+    trim: true,
+  },
+  email: {
+    required: true,
+    type: String,
+    trim: true,
+    validate: {
+      validator: (value) => {
+        const re =
+          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return value.match(re);
+      },
+      message: "Please enter a valid email address",
+    },
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+  address: {
+    type: String,
+    default: '',
+  },
+  type: {
+    type: String,
+    default: 'user',
+  },
 });
 
-const User = mongoose.model("User",userSchema);
-module.exports=User;
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,17 +1,37 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 const authRouter = express.Router();
 
-authRouter.post('/api/signup',async  (req,res)=>{
-const {name, email,password}=req.body;
+authRouter.post('/api/signup', async (req, res) => {
+  const { name, email, password } = req.body;
 
-const existingUser = await User.findOne({ email });
-if(existingUser){
-return res.status(400).json ({ msg: "User with same email already exists!"});
-}
+  // Log the received data for debugging
+  console.log('Received data:', { name, email, password });
 
+  if (!email || !password) {
+    return res.status(400).json({ msg: 'Please provide both email and password.' });
+  }
 
+  try {
+    // Hash the password before saving
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    let user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    user = await user.save();
+    res.json(user);
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ msg: 'Internal Server Error' });
+  }
 });
 
 module.exports = authRouter;
+
+
